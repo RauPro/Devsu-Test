ad9b9762df38a965f00034e5877db9c19e1aab7c
Raul Ernesto Guillen Hernandez <00012119@uca.edu.sv>
2023-10-05 21:56:10 -0600

    Add: OnDestroy hook

--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,45 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {IProduct} from "../../models/product.model";
-import {environment} from "../../../../../environments/environment";
-import {ProductService} from "../../services/product.service";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IProduct } from "../../models/product.model";
+import { ProductService } from "../../services/product.service";
 import { Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-
-
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy {
   products: IProduct[] = [];
   originalProducts: IProduct[] = [];
   private searchSubject = new Subject<string>();
+  private destroy$ = new Subject<void>();
 
-  constructor(private productService: ProductService) {
-  }
+  constructor(private productService: ProductService) {}
+
   ngOnInit(): void {
-    console.log(environment.baseUrl);
-    this.productService.getAllProducts().subscribe((products) => {
-      console.log(products);
-      this.products = products;
-      this.originalProducts = products;
-    })
+    this.productService.getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((products) => {
+        this.products = products;
+        this.originalProducts = products;
+      });
+
     this.searchSubject.pipe(
-      debounceTime(2000), // 2 seconds -- to save resources
-      distinctUntilChanged() // if value changes
+      debounceTime(2000), // Wait 2 seconds after the last event before emitting last event
+      distinctUntilChanged(), // Only emit if value is different from previous value
+      takeUntil(this.destroy$)
     ).subscribe(term => {
-      console.log(term)
-      if (term != ""){
-        this.products = this.products.filter(product => product.name.includes(term));
-      }
-      else {
-        this.products = this.originalProducts;
+      if (term.trim() !== "") {
+        this.products = this.originalProducts.filter(product => product.name.includes(term));
+      } else {
+        this.products = [...this.originalProducts];
       }
     });
   }
+
   handleSearch(term: string) {
     this.searchSubject.next(term);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
