eff9891de59c066a4f2e224fef1eb6f7caaaccfa
Raul Ernesto Guillen Hernandez <00012119@uca.edu.sv>
2023-10-05 22:58:28 -0600

    Add: Pagination

--- src/app/modules/products/components/product-table/product-table.component.ts
+++ src/app/modules/products/components/product-table/product-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {IProduct} from "../../models/product.model";
 
 @Component({
@@ -6,6 +6,56 @@ import {IProduct} from "../../models/product.model";
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.scss']
 })
-export class ProductTableComponent {
+export class ProductTableComponent implements OnChanges{
   @Input() products: IProduct[] = [];
+  itemsPerPageOptions = [5, 10, 15];
+  itemsPerPage = this.itemsPerPageOptions[0]; // Show 5 by default
+  currentPage = 1;
+  totalPages = 0;
+  paginatedProducts: IProduct[] = [];
+
+  ngOnChanges(): void {
+    this.totalPages = Math.ceil(this.products.length / this.itemsPerPage);
+    this.paginatedProducts = this.products.slice(0, this.itemsPerPage);
+  }
+
+  updatePagination() {
+    this.totalPages = Math.ceil(this.products.length / this.itemsPerPage);
+    if (this.currentPage > this.totalPages) {
+      this.currentPage = this.totalPages;
+    }
+    this.paginate();
+  }
+
+  paginate() {
+    const startItem = (this.currentPage - 1) * this.itemsPerPage;
+    const endItem = this.currentPage * this.itemsPerPage;
+    this.paginatedProducts = this.products.slice(startItem, endItem);
+  }
+  goToNextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginate();
+    }
+  }
+
+  goToPreviousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginate();
+    }
+  }
+
+  goToFirstPage() {
+    this.currentPage = 1;
+    this.paginate();
+  }
+
+  goToLastPage() {
+    this.currentPage = this.totalPages;
+    this.paginate();
+  }
+
+
+
 }
