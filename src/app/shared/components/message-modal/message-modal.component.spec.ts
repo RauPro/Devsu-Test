4d4b719ec748303e9a5713811cd1403be5b0cd0c
Raul Ernesto Guillen Hernandez <00012119@uca.edu.sv>
2023-10-06 22:20:56 -0600

    Test: Shared component

--- src/app/shared/components/message-modal/message-modal.component.spec.ts
+++ src/app/shared/components/message-modal/message-modal.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MessageModalComponent } from './message-modal.component';
 
 describe('MessageModalComponent', () => {
@@ -17,5 +16,60 @@ describe('MessageModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should display the provided title and message', () => {
+    component.title = 'Test Title';
+    component.message = 'Test Message';
+
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('.title-class').textContent).toContain('Test Title');
+    expect(compiled.querySelector('.message-class').textContent).toContain('Test Message');
+  });
+
+  it('should emit cancel event when cancel button is clicked', () => {
+    spyOn(component.cancel, 'emit');
+
+    const cancelButton = fixture.debugElement.nativeElement.querySelector('.close_btn');
+    cancelButton.click();
+
+    expect(component.cancel.emit).toHaveBeenCalled();
+  });
+
+  it('should emit confirm event when confirm button is clicked', () => {
+    spyOn(component.confirm, 'emit');
+
+    const confirmButton = fixture.debugElement.nativeElement.querySelector('.accept_btn');
+    confirmButton.click();
+
+    expect(component.confirm).toHaveBeenCalled();
+  });
+  it('should emit cancel event when onCancel() is called', () => {
+    let emitted = false;
+    component.cancel.subscribe(() => emitted = true);
+
+    component.onCancel();
+
+    expect(emitted).toBe(true);
+  });
+
+  it('should emit confirm event when onConfirm() is called', () => {
+    let emitted = false;
+    component.confirm.subscribe(() => emitted = true);
+
+    component.onConfirm();
+
+    expect(emitted).toBe(true);
+  });
+
+  it('should emit cancel event when onBackgroundClick() is called', () => {
+    let emitted = false;
+    component.cancel.subscribe(() => emitted = true);
+
+    component.onBackgroundClick();
+
+    expect(emitted).toBe(true);
   });
 });
